b5f1a4ded8545421ee4fd0654fc2f60202d5522a
Leha <leha.feygelman@gmail.com>
2024-01-27 13:58:48 +0300

    magic

---
+++ calculator_confetwish.py
@@ -0,0 +1,39 @@
+import calculator
+import confet
+import wish
+import login_pass
+
+access =False
+while True:
+    if not access:
+        print("programa ymeett")
+        print("1 - zaregatcia")
+        print("2 doiti in programy")
+        print("3 - dieti iz programi")
+        action = input("bedide nomer")
+        if action == "1":
+            login_pass.sign_up()
+        elif action =="2":
+            access =  login_pass.sign_in()
+        elif action == "3":
+            print("spacibo")
+            break
+    elif access:
+        print("menu")
+        print("1 - calculator")
+        print("2 - wish")
+        print("3 - confet")
+        print("4 - login")
+        print("5 - pass")
+        action = input("bedide nomer")
+        if action == "1":
+            calculator.main()
+        if action == "2":
+            wish.main()           
+        if action == "3":
+            confet.main()
+    input("nazmi enter i prodolzi")
+    if action == "4":
+            login_pass.open()
+    if action == "5":
+            login_pass.open() 
---
+++ login_pass.py
@@ -0,0 +1,36 @@
+def write_to_file(filename, text):
+    with open("text.txt",mode="a",encoding="utf-8") as file:
+        file.write(text)
+        
+def read_list(filename):
+    with open(filename, mode="r", encoding="utf-8") as file:
+        text = file.read().split("\n")
+        return text
+
+def sign_up():
+    login = input("login bedi")
+    password1 = input("pridymai parol")
+    password2 = input("bedide eoe jlby hfp")
+    if password1 == password2:
+        write_to_file("logins.txt",login+"\n")
+        write_to_file("passwords.txt",password1+"\n")
+        print(f"polzodatel {login} zaregal")
+    else:
+        print("paroli ne poxozi")
+        
+    import file1 
+        
+def sign_in():
+    login = input("Bedite login")
+    password = input("bedite parol")
+    login_list = read_list("logins.txt")
+    password_list = read_list("password.txt")
+    if login in login_list:
+        login_str = login_list.index(login)
+        True_password = password_list[login_str]
+        if password == True_password:
+            print("yra ti zachel")
+            return True
+        else:
+            print("parol inocorect")
+            return False

4bdb27ca28fdcbed1fb55c7c6638af056afeaea9
Leha <leha.feygelman@gmail.com>
2024-02-24 14:27:43 +0300

    microstate

--- bruhhh.py
+++ bruhhh.py
@@ -1,15 +1,15 @@
 import random
 
 class Fither:
-    def __init__(self,name:str.name_fatality:str):
-    self,name = name
-    self,power = random.radiant(1,5)
-    self,hp = 100
-    self,name_fat = name_fatality
+    def __init__(self, name:str, name_fatality:str):
+        self.name = name
+        self.power = random.radiant(1,5)
+        self.hp = 100
+        self.name_fat = name_fatality
     
 Fither1 = Fither("skorp","syper makak")
 Fither2 = Fither("oladyshki","cok")
 Fither3 = Fither("kyng fy panada","skill")
 
-Fithers -[Fither1,Fither2,Fither3]
+Fithers =[Fither1,Fither2,Fither3]
     
--- oop_country.py
+++ oop_country.py
@@ -14,9 +14,14 @@ class Country:
         print(f"{self.name} армия {self.army}")
 
 c1 = Country("russia", 10000000,300000,"medvediev",15)       
-c2 = Country("папаская область", 500,3000000,"italy",665)      
-c3 = Country("uzbek", 1,3000000000000,"uzbek",1)      
+c2 = Country("папаская область(ватикан)", 500,3000000,"italy",665)      
+c3 = Country("uzbek", 1,3000000000000,"uzbek",1) 
 
 c3.make_war(c1.name,"война за кортошка!!!!")
 c3.make_war(c1.name,"Mirok,")
 c1.army_conscription(1000000000)
+
+class Microstate(Country):
+    def __init__(self, name: str, gdp: int, leader: str, population: int):
+        super().__init__(name, gdp, 0, leader, population)
+

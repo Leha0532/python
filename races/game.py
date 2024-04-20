f06090747b009d41422638412f2df622471e9cd0
Leha <leha.feygelman@gmail.com>
2024-04-20 14:03:48 +0300

    cucumber

--- races/game.py
+++ races/game.py
@@ -7,23 +7,40 @@ SCREEN_TITLE = "ГОНКА"
 BG_IMG = "races/g.png"
 
 HERO_IMG = "races/car.png"
-HERO_SIZE = 0.35
-HERO_SPEED = 5
+HERO_SIZE = 0.25
+HERO_SPEED = 10
 HERO_ANGLE = 90
+CUCUMBER_IMG = 'races/cu.png'
+CUCUMBER_SIZE = 0.1
+
+class Cu(arcade.Sprite):
+    def update(self):
+        return super().update()
+    
 
 class Hero(arcade.Sprite):
     def update(self):
         self.center_x += self.change_x
+        m = 30
+        if self.center_x  > SCREEN_WIDTH - m:
+            self.center_x = SCREEN_WIDTH - m
+
+        if self.center_x  < 0 + m:
+            self.center_x = 0 + m
 
 class GameWindow(arcade.Window):
     def __init__(self,widht, height, title):
         super().__init__(widht, height, title)
         self.bg = arcade.load_texture(BG_IMG)
         self.hero = Hero(HERO_IMG, HERO_SIZE)
+        self.cu = Cu(CUCUMBER_IMG, CUCUMBER_SIZE)
+        
         
     def setup(self):
         self.hero.center_x = SCREEN_WIDTH/2 + 70
-        self.hero.center_y = SCREEN_HEIGHT/12
+        self.hero.center_y = SCREEN_HEIGHT/12 + 70
+        self.cu.center_x = 500
+        self.cu.center_y = 500
         self.hero.angle = 90
         self.hero.angle = HERO_ANGLE
     
@@ -35,6 +52,7 @@ class GameWindow(arcade.Window):
                                       height=SCREEN_HEIGHT,
                                       texture=self.bg)
         self.hero.draw()
+        self.cu.draw()
         
     def on_update(self, delta_time: float):
         self.hero.update()
@@ -42,7 +60,7 @@ class GameWindow(arcade.Window):
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.RIGHT:
             self.hero.change_x = HERO_SPEED
-            self.hero.angle = HERO_ANGLE - 20
+            self.hero.angle = HERO_ANGLE - 20 
         if symbol == arcade.key.LEFT:
             self.hero.change_x = - HERO_SPEED
             self.hero.angle = HERO_ANGLE + 20
@@ -52,6 +70,6 @@ class GameWindow(arcade.Window):
             self.hero.change_x = 0
             self.hero.angle = HERO_ANGLE
     
-window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, "Пинг-понг")
+window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 window.setup()
 arcade.run()
